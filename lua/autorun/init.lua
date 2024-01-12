85ca94a7e1ac6c9def85d1fdc1167bd4861f8e42
waternksy <playgamesroyale2@gmail.com>
2024-01-12 19:59:44 +0530

    feat: Added functionality to close the floating window

--- lua/autorun/init.lua
+++ lua/autorun/init.lua
@@ -1,15 +1,20 @@
+local closeFloatingWindow = function (bufnr)
+    local config = vim.api.nvim_win_get_config(bufnr)
+    if config.relative ~= "" then
+        vim.api.nvim_win_close(bufnr, false)
+    end
+end
+
 local createWin = function (lines)
 
     local width = 50
     local height = 10
 
     local bufnr = vim.api.nvim_create_buf(true, true)
-    --vim.api.nvim_buf_set_keymap(bufnr, "n", "<C-c>", [[vim.cmd.close]], {
-    --    silent = true,
-    --    nowait = true,
-    --    noremap = true,
-    --})
     vim.api.nvim_buf_set_lines(bufnr, 0, -1, true, lines)
+    vim.keymap.set("n", "<C-c>", function ()
+        closeFloatingWindow(vim.fn.win_findbuf(bufnr)[1])
+    end)
 
     local opts = {
         relative = "editor",
@@ -26,6 +31,23 @@ local createWin = function (lines)
     vim.api.nvim_open_win(bufnr, true, opts)
 end
 
+--[[
+local CloseAllFloatingWindows = function ()
+    local closed_windows = {}
+    for _, win in ipairs(vim.api.nvim_list_wins()) do
+        local config = vim.api.nvim_win_get_config(win)
+        if config.relative ~= "" then
+            vim.api.nvim_win_close(win, false)
+            table.insert(closed_windows, win)
+        end
+    end
+    print(string.format("Closed %d window: %s", #closed_windows, vim.inspect(closed_windows)))
+end
+
+vim.keymap.set("n", "<C-c>", function ()
+    CloseAllFloatingWindows()
+end)
+]]
 vim.api.nvim_create_autocmd("BufWritePost", {
     group = vim.api.nvim_create_augroup("Autorun", { clear = true }),
     pattern = "**/*.go",
